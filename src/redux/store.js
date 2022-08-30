36ba5ce88d566f537df1f8d5aba9368575b3ebe2
Konstantin Kotenko <90977909+Konstantin-Kotenko@users.noreply.github.com>
2022-08-30 12:49:21 +0300

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { middleware } from './middleware';
 import { rootReducer } from './contacts/contacts';
 
-export const store = configureStore(
-    rootReducer,
+export const store = configureStore({
+    reducer:{rootReducer,}
   middleware: middleware,
 });
